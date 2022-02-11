ff81580a62bff522c65dc83671789919040677c9
Nori <nrtkb@me.com>
2022-02-11 22:48:41 +0900

    コメントを追加

--- app/src/main/java/com/example/appstudy17class/MainActivity.kt
+++ app/src/main/java/com/example/appstudy17class/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Add comment
         Log.d("TAG","テスト")
     }
 }
